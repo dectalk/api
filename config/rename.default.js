105ddd09bb3e0e7504947be69ffedd1e733dd79c
lepon01 <leondrolio@gmail.com>
2017-06-05 21:31:11 +0100

    Update template default configuration file

--- config/rename.default.js
+++ config/rename.default.js
@@ -8,11 +8,19 @@ module.exports = {
 		clientSecret: '',
 		scope: [
 			'identify'
-		]
+		],
+		webhook: ""
 	},
+	reddit: {
+		clientID: '',
+		clientSecret: ''
+	},
+	name: 'DECtalk Online',
 	url: '127.0.0.1',
+	callback: 'http://127.0.0.1:3472/auth/login/callback',
+	useragent: 'DECtalk Online (https://talk.moustacheminer.com/)',
 	ports: {
-		http: 3472
+		http: 8080
 	},
 	rethinkdb: {
 		host: '',
@@ -20,6 +28,39 @@ module.exports = {
 		db: 'dectalk'
 	},
 	session: {
-		secret: 'Insert any RANDOM string into here. Maybe use random.org, /dev/urandom, or cosmic background radiation.'
+		secret: 'Add any random string here'
+	},
+	admins: [
+		"190519304972664832@discord"
+	],
+	limits: {
+		id: {
+			min: 30,
+			max: 40
+		},
+		name: {
+			min: 5,
+			max: 140
+		},
+		author: {
+			min: 3,
+			max: 140
+		},
+		artist: {
+			min: 3,
+			max: 140
+		},
+		dectalk: {
+			min: 5,
+			max: 10000
+		},
+		csrf: {
+			min: 127,
+			max: 129
+		},
+		other: {
+			min: 0,
+			max: 50
+		}
 	}
 };
