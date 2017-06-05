89b2ca92e44881fcc0bc1021e27689b93f92ab6d
lepon01 <leondrolio@gmail.com>
2017-06-05 13:18:02 +0100

    settimeout render

--- server/render.js
+++ server/render.js
@@ -12,33 +12,35 @@ setTimeout(()=>{
 
 				cursor.toArray((err, result) => {
 					if (err) return res.send(500, {error: err.message});
-					result.forEach((elem)=>{
+					result.forEach((elem, i)=>{
+						setTimeout(()=>{
+							fs.stat(`client\\dec\\${elem.id}.wav`, function(err, stat) {
+								//If the file doesn't exist, do a DECtalk
+								if((err && err.code == 'ENOENT')) {
+									console.log(`Processing ${elem.id}`);
 
-						fs.stat(`client\\dec\\${elem.id}.wav`, function(err, stat) {
-							//If the file doesn't exist, do a DECtalk
-							if((err && err.code == 'ENOENT')) {
-								console.log(`Processing ${elem.id}`);
+									//Make a temp file to store the file
+									tmp.file((err1, path, fd, clean) => {
 
-								//Make a temp file to store the file
-								tmp.file((err1, path, fd, clean) => {
+										if (err1) throw err1;
 
-									if (err1) throw err1;
+										//Write the message to the temp file
+										fs.writeFile(path, "[:phone on]" + elem.dectalk, (err2) => {
 
-									//Write the message to the temp file
-									fs.writeFile(path, "[:phone on]" + elem.dectalk, (err2) => {
+											if (err2) throw err2;
 
-										if (err2) throw err2;
-
-										//Grab the file, and write it into the dec folder
-										exec(`type ${path} | say -w client\\dec\\${elem.id}.wav`, (err3) => {
-											if (err3) throw err3;
+											//Grab the file, and write it into the dec folder
+											exec(`type ${path} | say -w client\\dec\\${elem.id}.wav`, (err3) => {
+												if (err3) throw err3;
+											});
 										});
 									});
-								});
-							} else {
-								console.log(`Skipping ${elem.id}`);
-							}
-						});
+								} else {
+									console.log(`Skipping ${elem.id}`);
+								}
+							});
+						}, i*200)
+
 					});
 				});
 
